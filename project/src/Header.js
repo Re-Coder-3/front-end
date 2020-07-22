f4d7ad4a3f6b619a9fc894e540ae12ff20114d98
miladiashe <dkdud0122@naver.com>
2020-07-22 16:55:35 +0900

    라우터 적용. 헤더와 푸터 생성. 쓸데없는 기본파일 삭제.

---
+++ project/src/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <div>
+            로고 검색창 로그인
+        </div>
+    );
+}
+
+// 헤더. 홈페이지의 맨 위에 들어가는 부분입니다.
+
+export default Header;
