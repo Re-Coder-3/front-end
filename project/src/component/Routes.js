78eff6d563833daccdf063e41fd3f8eadb512332
ehddnr301 <dy950328@gmail.com>
2020-08-08 14:46:26 +0900

    slide

--- project/src/component/Routes.js
+++ project/src/component/Routes.js
@@ -2,10 +2,13 @@ import React from "react";
 import { Route, Switch, Redirect } from "react-router-dom";
 import Auth from "../page/Auth";
 import Home from "../page/Home";
+import Main from "../page/Main";
 
 const LoggedOutPage = () => (
   <Switch>
     <Route path="/auth" component={Auth} />
+    <Route path="/slide" component={Main} />
+
     {/* <Route path="/extraAuth" component={ExtraAuth} /> */}
     <Redirect from="*" to="/" />
   </Switch>
