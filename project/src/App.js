3ba71df8e2fa01883b6a53c7d16c8159125a4421
김서영 <55133538+ksy990628@users.noreply.github.com>
2020-08-08 15:42:11 +0900

    hello

--- project/src/App.js
+++ project/src/App.js
@@ -1,21 +1,16 @@
-
-import React from 'react';
-import { Route } from 'react-router-dom'
-import Main from "./page/Main";
-import Profile from "./page/Profile";
-import Profile3 from "./component/Profile3";
-import Login from "./component/Login";
+import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import GlobalStyles from "./component/GlobalStyles";
+import Routes from "./component/Routes";
 
 function App() {
   return (
-      <switch>
-    <div>
-        <Route path="/" component={Main} exact={true}/>
-        <Route path="/profile"  component={Profile} />
-        <Route path="/profileportfolio" component={Profile3} />
-        <Route path="/login" component={Login}    />
-    </div>
-      </switch>
+    <>
+      <GlobalStyles />
+      <Router>
+        <Routes LoggedBool={true} />
+      </Router>
+    </>
   );
 }
 
