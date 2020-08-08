ee7c5e5417b9b13927b014dcc0e3ef064afff97d
miladiashe <dkdud0122@naver.com>
2020-08-09 00:01:37 +0900

    footer 글씨색 변경

--- project/src/component/Footer.js
+++ project/src/component/Footer.js
@@ -1,10 +1,21 @@
 import React from "react";
+import styled from "styled-components";
+
+const Foot = styled.div`
+  height: 7vw;
+  border-top: solid 1px #747474;
+`
+
+const FootText = styled.div`
+  font-size: 1.4vw;
+  color: grey;
+`
 
 const Footer = () => {
     return (
-        <div>
-            영문로고 회사소개 이용안내 개인정보처리방침 Q/A 고객센터 신고하기
-        </div>
+        <Foot>
+            로고 <FootText>회사소개 이용안내 개인정보처리방침 Q/A 고객센터 신고하기</FootText>
+        </Foot>
     );
 }
 
