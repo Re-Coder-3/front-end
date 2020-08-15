43ddffef7b9583f75c97edc613faf9b86ad07242
ehddnr301 <dy950328@gmail.com>
2020-08-15 18:18:23 +0900

    Feature - upload success

--- project/src/component/ClientState.js
+++ project/src/component/ClientState.js
@@ -1,18 +1,42 @@
-export default {
-  defaults: {
-    LoggedBool: localStorage.getItem("TOKEN") ? true : false,
-  },
-  resolvers: {
-    Mutation: {
-      loginState: (_, { token }, { cache }) => {
-        localStorage.setItem("TOKEN", token);
-        cache.writeData({
-          data: {
-            LoggedBool: true,
-          },
-        });
-        return null;
-      },
+import { gql } from "apollo-boost";
+
+// export default {
+//   defaults: {
+//     LoggedBool: localStorage.getItem("TOKEN") ? true : false,
+//   },
+//   resolvers: {
+//     Mutation: {
+//       loginState: (_, { token }, { cache }) => {
+//         localStorage.setItem("TOKEN", token);
+//         cache.writeData({
+//           data: {
+//             LoggedBool: true,
+//           },
+//         });
+//         return null;
+//       },
+//     },
+//   },
+// };
+
+// export const defaults = {
+//   LoggedBool: localStorage.getItem("TOKEN") ? true : false,
+// };
+export const typeDefs = gql`
+  extend type Query {
+    LoggedBool: Boolean!
+  }
+`;
+export const resolvers = {
+  Mutation: {
+    loginState: (_, { token }, { cache }) => {
+      localStorage.setItem("TOKEN", token);
+      cache.writeData({
+        data: {
+          LoggedBool: true,
+        },
+      });
+      return null;
     },
   },
 };
--- project/src/index.js
+++ project/src/index.js
@@ -2,22 +2,31 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
-import ClientState from "./component/ClientState";
+import { resolvers, typeDefs } from "./component/ClientState";
 
-import ApolloClient from "apollo-boost";
+import ApolloClient from "apollo-client";
 // import { ApolloClient } from "apollo-boost-upload";
 import { ApolloProvider, ApolloLink } from "@apollo/react-hooks";
 import { InMemoryCache } from "apollo-cache-inmemory";
 import { createUploadLink } from "apollo-upload-client";
 
+const cache = new InMemoryCache();
+
 const client = new ApolloClient({
   uri: "http://localhost:5000/",
-  clientState: ClientState,
   headers: {
     Authorization: `${localStorage.getItem("TOKEN")}`,
   },
-  link: ApolloLink.split([createUploadLink({ uri: "http://localhost:5000" })]),
-  cache: new InMemoryCache(),
+  link: createUploadLink({ uri: "http://localhost:5000" }),
+  cache,
+  typeDefs,
+  resolvers,
+});
+
+cache.writeData({
+  data: {
+    LoggedBool: localStorage.getItem("TOKEN"),
+  },
 });
 
 ReactDOM.render(
