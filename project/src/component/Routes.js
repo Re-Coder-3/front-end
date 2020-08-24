e9aa9b3a44d4969610975b72e9d714cda8a1252c
ehddnr301 <dy950328@gmail.com>
2020-08-24 22:13:54 +0900

    Fix - Add BrowserRouter

--- project/src/component/Routes.js
+++ project/src/component/Routes.js
@@ -1,5 +1,10 @@
 import React from "react";
-import { Route, Switch, Redirect } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from "react-router-dom";
 import Auth from "../page/Auth";
 import Home from "../page/Home";
 import Main from "../page/Main";
@@ -9,30 +14,34 @@ import Profile from "../page/Profile";
 import MyPage from "../page/MyPage";
 
 const LoggedOutPage = () => (
-  <>
-    <Header />
-    <Switch>
-      <Route exact path="/" component={Home} />
-      <Route path="/auth" component={Auth} />
-      <Route path="/search" component={Search} />
-      <Route path="/profile" component={Profile} />
-      <Route path="/mypage" component={MyPage} /> {/* 시험용 */}
-      {/* <Route path="/extraAuth" component={ExtraAuth} /> */}
-      <Redirect from="*" to="/" />
-    </Switch>
-  </>
+  <Router>
+    <>
+      <Header />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/auth" component={Auth} />
+        <Route path="/search" component={Search} />
+        <Route path="/profile" component={Profile} />
+        <Route path="/mypage" component={MyPage} /> {/* 시험용 */}
+        {/* <Route path="/extraAuth" component={ExtraAuth} /> */}
+        <Redirect from="*" to="/" />
+      </Switch>
+    </>
+  </Router>
 );
 
 const LoggedInPage = () => (
-  <>
-    <Header />
-    <Switch>
-      <Route exact path="/" component={Main} />
-      <Route path="/search" component={Search} />
-      <Route path="/mypage" component={MyPage} />
-      <Redirect from="*" to="/" />
-    </Switch>
-  </>
+  <Router>
+    <>
+      <Header />
+      <Switch>
+        <Route exact path="/" component={Main} />
+        <Route path="/search" component={Search} />
+        <Route path="/mypage" component={MyPage} />
+        <Redirect from="*" to="/" />
+      </Switch>
+    </>
+  </Router>
 );
 
 export default ({ LoggedBool }) =>
