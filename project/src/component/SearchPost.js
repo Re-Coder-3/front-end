e2385825038925f5f6ca4aece1f22b5aeb8ec928
miladiashe <dkdud0122@naver.com>
2020-08-27 15:38:29 +0900

    검색결과창 CSS 추가

--- project/src/component/SearchPost.js
+++ project/src/component/SearchPost.js
@@ -14,6 +14,14 @@ const Post = styled.div`
 const Content = styled.div`
   width:65%;
   padding: 1vw 2vw;
+  display: grid;
+  grid-template-rows:2fr 1.5fr 1fr 4.5fr;
+  grid-template-columns:1fr 5fr;
+  grid-template-areas:
+  "profile Title"
+  "profile Tags"
+  "profile Etc"
+  "ContentText ContentText";
 `
 const ImgWrapper = styled.div`
   background-color: #333aaa;
@@ -25,11 +33,50 @@ const ImgWrapper = styled.div`
 const Image = styled.img`
   max-width: 100%;
 `
+const Profile = styled.div`
+  grid-area: profile;
+  text-align: center;
+`
+const Name = styled.div`
+  padding:5%;
+  font-size: 1vw;
+`
+const Title = styled.div`
+  grid-area: Title;
+  font-size: 1.5vw;
+`
+const Tags = styled.div`
+  grid-area: Tags;
+  font-size: 1.3vw;
+`
+const Etc = styled.div`
+  grid-area: Etc;
+  font-size: 1vw;
+`
+const ContentText = styled.div`
+  grid-area: ContentText;
+  font-size: 1vw;
+`
+const ProfileImgWrapper = styled.div`
+overflow: hidden;
+border-radius: 100%;
+width: 70%;
+height: 70%;
+border: solid 1px black;
+margin: 0 auto;
+`
 
 const SearchPost = () => {
     return (
         <Post>
-            <Content>사진 사람이름 태그 제목 지역 분야 필수조건 글내용미리보기</Content><ImgWrapper><Image src={testimg} /></ImgWrapper>
+            <Content>
+                <Profile><ProfileImgWrapper><Image src={testimg} /></ProfileImgWrapper> <Name>사람이름</Name></Profile>
+                <Title>제목</Title>
+                <Tags>태그</Tags>
+                <Etc>지역 분야 필수조건</Etc>
+                <ContentText>글내용미리보기</ContentText>
+            </Content>
+            <ImgWrapper><Image src={testimg} /></ImgWrapper>
         </Post>
     );
 };
