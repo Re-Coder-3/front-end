10933c222e5163c8ddf07c0012a5e10da65c7732
ehddnr301 <dy950328@gmail.com>
2020-09-04 18:34:40 +0900

    Feature - update url use global state

--- project/src/component/ClientState.js
+++ project/src/component/ClientState.js
@@ -1,30 +1,15 @@
 import { gql } from "apollo-boost";
 
-// export default {
-//   defaults: {
-//     LoggedBool: localStorage.getItem("TOKEN") ? true : false,
-//   },
-//   resolvers: {
-//     Mutation: {
-//       loginState: (_, { token }, { cache }) => {
-//         localStorage.setItem("TOKEN", token);
-//         cache.writeData({
-//           data: {
-//             LoggedBool: true,
-//           },
-//         });
-//         return null;
-//       },
-//     },
-//   },
-// };
+const query = gql`
+  query url {
+    UrlArr @client
+  }
+`;
 
-// export const defaults = {
-//   LoggedBool: localStorage.getItem("TOKEN") ? true : false,
-// };
 export const typeDefs = gql`
   extend type Query {
     LoggedBool: Boolean!
+    UrlArr: [String]
   }
 `;
 export const resolvers = {
@@ -37,6 +22,24 @@ export const resolvers = {
         },
       });
       return null;
+    },
+    addUrl: (_, { url }, { cache }) => {
+      const { UrlArr } = cache.readQuery({ query });
+      cache.writeData({
+        data: {
+          UrlArr: [...UrlArr, url],
+        },
+      });
+      console.log(UrlArr);
+    },
+    removeUrl: (_, { bool }, { cache }) => {
+      if (bool) {
+        cache.writeData({
+          data: {
+            UrlArr: [],
+          },
+        });
+      }
     },
   },
 };
