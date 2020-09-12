b5eb4fb7d33048ace607e6be93fc609009851e0e
miladiashe <dkdud0122@naver.com>
2020-09-12 16:50:31 +0900

    푸터 로고에도 링크담

--- project/src/component/Footer.js
+++ project/src/component/Footer.js
@@ -1,6 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import {TitleLogo} from "./Icons";
+import {Link} from "react-router-dom";
 
 const Foot = styled.div`
   height: 7vw;
@@ -27,7 +28,8 @@ const Logo = styled.div`
 const Footer = () => {
     return (
         <Foot>
-            <Logo><TitleLogo/>  커리어스팟</Logo><FootText>회사소개 이용안내 개인정보처리방침 Q/A 고객센터 신고하기</FootText>
+            <Logo><Link to={"/"} style={{ textDecoration: 'none', color:"black"}}><TitleLogo/></Link><Link to={"/"} style={{ textDecoration: 'none', color:"black"}}>  커리어스팟</Link></Logo>
+            <FootText>회사소개 이용안내 개인정보처리방침 Q/A 고객센터 신고하기</FootText>
         </Foot>
     );
 }
